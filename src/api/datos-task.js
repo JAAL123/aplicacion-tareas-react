b24b8d5d44eeda884dc7d897d359a5c37e9e7093
Jose Aguilar <JAAL0254@gmail.com>
2022-12-08 17:27:51 -0600

    pagina de tareas

---
+++ src/api/datos-task.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primer tarea",
+    description: "Esta es la primer tarea",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "Esta es la segunda tarea",
+  },
+  {
+    id: 2,
+    title: "Mi tercer tarea",
+    description: "Esta es la tercer tarea",
+  },
+];
